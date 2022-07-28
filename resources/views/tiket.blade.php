f6f725bd8102b66e153462c59e6e8f2409588f57
Ayi <ayianjani774@gmail.com>
2022-07-28 07:44:22 +0700

    tambah tamplates

---
+++ resources/views/tiket.blade.php
@@ -0,0 +1,34 @@
+<!-- works -->
+<div class="works">
+    <div class="container">
+       <div class="row">
+          <div class="col-md-12">
+             <div class="titlepage">
+                <span>How it works</span>
+                <h2> <strong class="yellow">3 Step For </strong>Healthy Eating</h2>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+          <div class="col-md-4">
+             <div id="white_bg" class="works_box">
+                <h4>01</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div id="white_bg" class="works_box ">
+                <h4>02</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div id="white_bg" class="works_box">
+                <h4>03</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+ <!-- end works -->
