691d5b40ad27e290daff95ffe4dbf1a33f6aa41d
Ayi <ayianjani774@gmail.com>
2022-07-28 07:45:34 +0700

    update views

---
+++ resources/views/pesan.blade.php
@@ -0,0 +1,35 @@
+<!-- banner -->
+<section class="banner_main">
+    <div class="container">
+    <div class="row d_flex">
+       <div class="col-md-6">
+          <div class="text-bg">
+             <h1>Healthy Food Recipes</h1>
+             <p>There are many variations of passages of Lorem Ipsum available, but the majorityomised words which don't look even slightly believable</p>
+             <a class="read_more" href="#">Read More</a>
+          </div>
+       </div>
+       <div class="col-md-6">
+          <form id="request" class="main_form">
+             <div class="row">
+                <div class="col-md-12 ">
+                   <input class="contactus" placeholder="Name" type="type" name="Name"> 
+                </div>
+                <div class="col-md-12">
+                   <input class="contactus" placeholder=" Email" type="type" name="Email"> 
+                </div>
+                <div class="col-md-12">
+                   <input class="contactus" placeholder=" Phone Number" type="type" name="Phone Number">                          
+                </div>
+                <div class="col-md-12">
+                   <textarea class="contactus" placeholder="Message" type="type" Message="Name">Message </textarea>
+                </div>
+                <div class="col-sm-12">
+                   <button class="send_btn">Send</button>
+                </div>
+             </div>
+          </form>
+       </div>
+    </div>
+ </section>
+ <!-- end banner -->
---
+++ resources/views/wisata.blade.php
@@ -0,0 +1,38 @@
+<!-- food -->
+<div class="food">
+    <div class="container">
+       <div class="row">
+          <div class="col-md-12">
+             <div class="titlepage">
+                <h2> <strong class="yellow">Food </strong>Packages</h2>
+                <span>There are many variations of passages of Lorem Ipsum available, but the majorityomised words which don't look 
+                even slightly believable</span>
+             </div>
+          </div>
+       </div>
+       <div class="row">
+          <div class="col-md-4">
+             <div class="food_box">
+                <i><img src="images/food1.png" alt="#"/></i>
+                <h4>Homemade</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div class="food_box ">
+                <i><img src="images/food2.png" alt="#"/></i>
+                <h4>Noodles</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+          <div class="col-md-4">
+             <div class="food_box">
+                <i><img src="images/food3.png" alt="#"/></i>
+                <h4>Egg</h4>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>
+ <!-- end food -->
