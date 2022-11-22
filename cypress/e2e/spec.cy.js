d6f23cb0782a53d797726c170ea4c619798de56f
anjanidwilestari <ayianjani774@gmail.com>
2022-11-22 20:46:13 +0700

    tambah test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -183,3 +183,22 @@ describe("mengisi form transaksi input pengunjung anak 0", () => {
         cy.contains("Lihat Detail").click();
     });
 });
+
+//test 12
+describe("mengisi form transaksi input nama lengkap invalid", () => {
+    it("failed", () => {
+        cy.visit("http://127.0.0.1:8000/transaksi");
+        cy.findByPlaceholderText("Nama Lengkap").type("~!@#$%^&*()_+");
+        cy.findByPlaceholderText("No Identitas").type("3575030000000000");
+        cy.findByPlaceholderText("No. HP").type("085231404775");
+        cy.get("select").select("Masjid Putih").should("have.value", "3");
+        cy.findByPlaceholderText("Tanggal Kunjungan").type("2022-11-22");
+        cy.findByPlaceholderText("Pengunjung Dewasa").type("3");
+        cy.findByPlaceholderText("Pengunjung Anak-Anak").type("3");
+        cy.findByPlaceholderText("Harga Tiket").type("30000");
+        cy.contains("Pesan Tiket").click();
+        cy.contains("OK").click();
+        cy.contains("Lihat History").click();
+        cy.contains("Lihat Detail").click();
+    });
+});
